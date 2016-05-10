c43b509920d0800257e88a50670bd50bd95f5383
Renato <renato.mello@paulistano.org.br>
2016-05-10 17:10:09 -0300

    Aula 2 concluída

---
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -0,0 +1,59 @@
+package br.com.casadocodigo.loja.models;
+
+public class Produto {
+	
+	private String titulo; 
+	
+	private String descricao;
+	
+	private int paginas;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getPaginas() {
+		return paginas;
+	}
+
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Produto other = (Produto) obj;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		return true;
+	}
+
+}
